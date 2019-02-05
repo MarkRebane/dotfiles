8cfdb3aff24b56fd8df9fc4b9b833cac8e7c096b
Mark Rebane <mark.rebane@gmail.com>
2019-02-05 21:59:38 +1030

    Scripts: Add install-tool-dependencies install script.

---
+++ scripts/install-tool-dependencies.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+sudo apt -y install libevent-dev libutempter-dev \
+autoconf automake pkg-config 
+
+sudo apt -y install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev \
+libxpm-dev libxt-dev python python-dev python3 python3-dev ruby-dev lua5.3 \
+liblua5.3-dev libperl-dev git libgpm-dev gpm cmake python-pip
+
+
+#pip install --user powerline-status
+
+# Create Symlinks if lua 5.3 isn't detected
+# TODO detect 5.3
+sudo ln -s /usr/include/lua5.3 /usr/local/include/lua
+sudo ln -s /usr/lib/x86_64-linux-gnu/liblua5.3.so /usr/local/lib/liblua.so
+sudo ln -s /usr/bin/lua5.3 /usr/local/bin/lua
