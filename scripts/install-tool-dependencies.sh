e3181fa0a94330b268f14137cb8b84ccc30238a8
Mark Rebane <mark.rebane@gmail.com>
2019-09-12 20:36:16 +0930

    scripts/install-tool-dependencies.sh: Add dependencies for Python.

--- scripts/install-tool-dependencies.sh
+++ scripts/install-tool-dependencies.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+sudo apt -y update
 sudo apt -y install libevent-dev libutempter-dev \
 autoconf automake pkg-config bison \
 libncurses5-dev libgnome2-dev libgnomeui-dev \
 libgtk2.0-dev libatk1.0-dev libbonoboui2-dev libcairo2-dev libx11-dev \
-libxpm-dev libxt-dev python python-dev python3 python3-dev ruby-dev lua5.3 \
-liblua5.3-dev libperl-dev git libgpm-dev gpm cmake python-pip curl \
-bash-completion git-extras
+libxpm-dev libxt-dev python python-dev python3 python3-dev python3-pip \
+ruby-dev lua5.3 liblua5.3-dev libperl-dev git libgpm-dev gpm cmake \
+curl bash-completion git-extras python3-venv
 
-
+pip3 install --user black
 #pip install --user powerline-status
 
 # Create Symlinks if lua 5.3 isn't detected
