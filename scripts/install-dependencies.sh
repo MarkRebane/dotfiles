eeecad915c47d0b32252c5b5f2cbd6cb486218cf
Mark Rebane <mark.rebane@gmail.com>
2020-05-22 02:05:52 +0930

    scripts/install-dependencies.sh: Add additional dependencies

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -2,13 +2,25 @@
 set -e
 
 sudo apt -y update
-sudo apt -y install autoconf automake bash-completion bison curl cmake         \
-pkg-config git git-extras git-lfs tig                                          \
-python3 python3-dev python3-pip python3-venv                                   \
-libevent-dev libutempter-dev libncurses5-dev                                   \
-libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev                           \
-libxpm-dev libxt-dev libperl-dev gpm libgpm-dev lua5.3 liblua5.3-dev           \
-ruby-dev
+
+# Basic (tmux/vim) dependencies
+sudo apt -y install                                                            \
+    autoconf automake bash-completion bison curl cmake                         \
+    pkg-config git git-extras git-lfs ninja-build                              \
+    python3 python3-dev python3-pip python3-venv                               \
+    libevent-dev libutempter-dev libncurses5-dev                               \
+    libgtk2.0-dev libatk1.0-dev libcairo2-dev libx11-dev                       \
+    libxpm-dev libxt-dev libperl-dev gpm libgpm-dev lua5.3 liblua5.3-dev       \
+    ruby-dev
+
+# Clang dependencies
+sudo apt -y install                                                            \
+    libz3-dev libxml2-dev libocaml-compiler-libs-ocaml-dev libedit-dev         \
+    liblzma-dev swig
+
+# Additional tools
+sudo apt -y install                                                            \
+    doxygen ripgrep tig
 
 # Python 3 auto-formatter.
 # pip3 install --user black
