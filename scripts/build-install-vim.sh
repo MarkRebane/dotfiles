b37e98cbc61332bbd9369f5c6f22364162b58b21
Mark Rebane <mark.rebane@gmail.com>
2020-05-22 00:42:46 +0930

    scripts/build-install-vim.sh: Simplify

--- scripts/build-install-vim.sh
+++ scripts/build-install-vim.sh
@@ -19,38 +19,9 @@ else
     cd "${vim_dir}"
 fi
 
-lua_prefix="/usr"
-if [ ! -d ${lua_prefix} ]; then
-    echo "Error! Lua prefix directory not found: ${lua_prefix}"
-    # TODO Look for environment variables.
-    exit 1
-fi
-
-# python2_config_dir="/usr/lib/python2.7/config-x86_64-linux-gnu"
-# if [ ! -d ${python2_config_dir} ]; then
-#     echo "Error! Python2.7 config not found: ${python2_config}"
-#     exit 1
-# fi
-
-python3_7_config_dir="/usr/lib/python3.7/config-3.7m-x86_64-linux-gnu"
-python3_6_config_dir="/usr/lib/python3.6/config-3.6m-x86_64-linux-gnu"
-python3_5_config_dir="/usr/lib/python3.5/config-3.5m-x86_64-linux-gnu"
-if [ ! -d ${python3_7_config_dir} ]; then
-    echo "Python3.7 config found: ${python3_7_config_dir}"
-    python3_config_dir=${python3_7_config_dir}
-elif [ ! -d ${python3_6_config_dir} ]; then
-    echo "Python3.6 config found: ${python3_6_config_dir}"
-    python3_config_dir=${python3_6_config_dir}
-elif [ ! -d ${python3_config_dir} ]; then
-    echo "Python3.5 config found: ${python3_5_config_dir}"
-    python3_config_dir=${python3_5_config_dir}
-else
-    echo "Error: couldn't find python3, aborting."
-    exit 1
-fi
-
 make distclean
-./configure --with-features=huge \
+./configure \
+    --with-features=huge \
     --enable-cscope \
     --enable-gpm \
     --enable-gui=gtk2 \
@@ -59,11 +30,8 @@ make distclean
     --enable-luainterp=yes \
     --enable-rubyinterp=yes \
     --enable-perlinterp=yes \
-\ #    --enable-pythoninterp=yes \
+    --enable-python2interp=yes \
     --enable-python3interp=yes \
-    --with-lua_prefix=${lua_prefix} \
-\ #    --with-python-config-dir="${python2_config_dir}" \
-    --with-python3-config-dir=${python3_config_dir} \
     --enable-fail-if-missing \
     --prefix="${HOME}"/.local/vim
 
