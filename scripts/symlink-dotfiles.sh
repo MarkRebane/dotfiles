84f57abc098b51f3081477c007f1a0062b0e9043
Mark Rebane <mark.rebane@gmail.com>
2025-12-19 12:46:13 +1030

    Change prefix . -> dot-

--- scripts/symlink-dotfiles.sh
+++ scripts/symlink-dotfiles.sh
@@ -1,53 +1,53 @@
 #!/bin/sh
 
-date=$(date --rfc-3339='seconds')
-source_dir="$(cd "$(dirname "$0")/.." && pwd)"
+DATE=$(date --rfc-3339='seconds')
+DOTFILES_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
 if [ -f ~/.bashrc ] && [ ! -h ~/.bashrc ] ; then
-  mv "$HOME/.bashrc" "$HOME/dot-bashrc-$date"
+    mv "$HOME/.bashrc" "$HOME/dot-bashrc-$DATE"
 fi
 if [ ! -h ~/.bashrc ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.bashrc" "$HOME/.bashrc"
+    ln -snf "$DOTFILES_DIR/dot-bashrc" "$HOME/.bashrc"
 fi
 
 if [ -f ~/.bash_logout ] && [ ! -h ~/.bash_logout ] ; then
-  mv "$HOME/.bash_logout" "$HOME/dot-bash_logout-$date"
+    mv "$HOME/.bash_logout" "$HOME/dot-bash_logout-$DATE"
 fi
 if [ ! -h ~/.bash_logout ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.bash_logout" "$HOME/.bash_logout"
+    ln -snf "$DOTFILES_DIR/dot-bash_logout" "$HOME/.bash_logout"
 fi
 
 if [ -f ~/.inputrc ] && [ ! -h ~/.inputrc ] ; then
-  mv "$HOME/.inputrc" "$HOME/dot-inputrc-$date"
+    mv "$HOME/.inputrc" "$HOME/dot-inputrc-$DATE"
 fi
 if [ ! -h ~/.inputrc ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.inputrc" "$HOME/.inputrc"
+    ln -snf "$DOTFILES_DIR/dot-inputrc" "$HOME/.inputrc"
 fi
 
 if [ -f ~/.profile ] && [ ! -h ~/.profile ] ; then
-  mv "$HOME/.profile" "$HOME/dot-profile-$date"
+    mv "$HOME/.profile" "$HOME/dot-profile-$DATE"
 fi
 if [ ! -h ~/.profile ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.profile" "$HOME/.profile"
+    ln -snf "$DOTFILES_DIR/dot-profile" "$HOME/.profile"
 fi
 
 if [ -f ~/.tmux.conf ] && [ ! -h ~/.tmux.conf ] ; then
-  mv "$HOME/.tmux.conf" "$HOME/dot-tmux.conf-$date"
+    mv "$HOME/.tmux.conf" "$HOME/dot-tmux.conf-$DATE"
 fi
 if [ ! -h ~/.tmux.conf ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.tmux.conf" "$HOME/.tmux.conf"
+    ln -snf "$DOTFILES_DIR/dot-tmux.conf" "$HOME/.tmux.conf"
 fi
 
 if [ -f ~/.vimrc ] && [ ! -h ~/.vimrc ] ; then
-  mv "$HOME/.vimrc" "$HOME/dot-vimrc-$date"
+    mv "$HOME/.vimrc" "$HOME/dot-vimrc-$DATE"
 fi
 if [ ! -h ~/.vimrc ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.vimrc" "$HOME/.vimrc"
+    ln -snf "$DOTFILES_DIR/dot-vimrc" "$HOME/.vimrc"
 fi
 
 if [ -d ~/.vim ] && [ ! -h ~/.vim ] ; then
-  mv "$HOME/.vim" "$HOME/dot-vim-$date"
+    mv "$HOME/.vim" "$HOME/dot-vim-$DATE"
 fi
 if [ ! -h ~/.vim ] || [ "$1" = "--force" ] ; then
-  ln -snf "$source_dir/.vim" "$HOME/.vim"
+    ln -snf "$DOTFILES_DIR/dot-vim" "$HOME/.vim"
 fi
