adb865bc65677171e33ed9bbbc4c0310e4042dd4
Mark Rebane <mark.rebane@gmail.com>
2025-12-19 12:20:33 +1030

    scripts/build-neovide.sh: Improve bash conventions

--- scripts/build-neovide.sh
+++ scripts/build-neovide.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-set -e
+set -euxo pipefail
 
 PACKAGE_NAME="neovide"
 PACKAGE_VERSION="0.15.2"
 INSTALL_EXE="$HOME/.local/bin/$PACKAGE_NAME"
 
-SOURCE_DIR="$HOME/source/tools/$PACKAGE_NAME"
-mkdir -p "$SOURCE_DIR"
-pushd "$SOURCE_DIR"
+SOURCE_DIR="$HOME/source/tools"
+mkdir -p "$SOURCE_DIR/$PACKAGE_NAME"
+pushd "$SOURCE_DIR/$PACKAGE_NAME"
 
 wget https://github.com/neovide/neovide/releases/download/$PACKAGE_VERSION/neovide-linux-x86_64.tar.gz
 tar xf neovide-linux-x86_64.tar.gz
@@ -15,5 +15,5 @@ mv neovide "$INSTALL_EXE"
 chmod u+x "$INSTALL_EXE"
 
 popd
-rm -r "$SOURCE_DIR"
+rm -r "$SOURCE_DIR/$PACKAGE_NAME"
 
