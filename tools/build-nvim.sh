02a84c8f7e323fcccec85037f6588cfd404e9c6b
Mark Rebane <mark.rebane@gmail.com>
2026-03-30 11:38:01 +1030

    tools/build-nvim.sh: Install stable release NVIM 0.12.0

--- tools/build-nvim.sh
+++ tools/build-nvim.sh
@@ -2,25 +2,17 @@
 set -euxo pipefail
 
 PACKAGE_NAME="nvim"
+PACKAGE_VERSION="0.12.0"
+INSTALL_DIR="$HOME/.local/$PACKAGE_NAME"
 
 SOURCE_DIR="$HOME/source/tools"
-mkdir -p "$SOURCE_DIR"
-
-if [ -d "$SOURCE_DIR/$PACKAGE_NAME" ]; then
-    pushd "$SOURCE_DIR/$PACKAGE_NAME"
-    git pull --rebase
-    popd
-else
-    pushd "$SOURCE_DIR"
-    git clone --recursive https://github.com/neovim/neovim "$PACKAGE_NAME"
-    popd
-fi
-
+mkdir -p "$SOURCE_DIR/$PACKAGE_NAME"
 pushd "$SOURCE_DIR/$PACKAGE_NAME"
 
-make distclean
-make deps
-make CMAKE_BUILD_TYPE=RelWithDebInfo
-make CMAKE_INSTALL_PREFIX="$HOME/.local/nvim" install
+wget https://github.com/neovim/neovim/releases/download/v$PACKAGE_VERSION/nvim-linux-x86_64.tar.gz
+tar xf nvim-linux-x86_64.tar.gz
+rm -rf "$INSTALL_DIR"
+mv nvim-linux-x86_64 "$INSTALL_DIR"
 
 popd
+rm -rf "$SOURCE_DIR/$PACKAGE_NAME"
