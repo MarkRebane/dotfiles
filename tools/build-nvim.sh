fe3130986f5ac4dd9c7113a4b76df8b6949d05e3
Mark Rebane <mark.rebane@gmail.com>
2026-04-10 15:45:09 +0930

    tools/build-nvim.sh: Use latest release

--- tools/build-nvim.sh
+++ tools/build-nvim.sh
@@ -2,15 +2,16 @@
 set -euxo pipefail
 
 PACKAGE_NAME="nvim"
-PACKAGE_VERSION="0.12.1"
 INSTALL_DIR="$HOME/.local/$PACKAGE_NAME"
 
 SOURCE_DIR="$HOME/source/tools"
 mkdir -p "$SOURCE_DIR/$PACKAGE_NAME"
 pushd "$SOURCE_DIR/$PACKAGE_NAME"
 
-wget https://github.com/neovim/neovim/releases/download/v$PACKAGE_VERSION/nvim-linux-x86_64.tar.gz
-tar xf nvim-linux-x86_64.tar.gz
+
+
+wget https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+tar xzvf nvim-linux-x86_64.tar.gz
 rm -rf "$INSTALL_DIR"
 mv nvim-linux-x86_64 "$INSTALL_DIR"
 
